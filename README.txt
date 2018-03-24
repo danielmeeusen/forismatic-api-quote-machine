a5d21458ad66e25c768c6a542f80c1bc8950f22f
Daniel Meeusen <danmeeusen@gmail.com>
2018-03-23 17:44:22 -0700

    update all

--- README.txt
+++ README.txt
@@ -1,4 +1,3 @@
-
 A Pen created at CodePen.io. You can find this one at https://codepen.io/dutchbones/pen/wroxNN.
 
-This site is published at https://dutchbones.github.io/forismatic-api-quote-machine/
+ Very basic Forismatic API quote generator done as part as part of the intermediate front end development projects for freecodecamp.com.  
--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
-$('#hidden').hide(0).delay(500).fadeIn(1000);
+
+  // Jquery to to allow the quotebox to fade in.
+  $('#hidden').hide(0).delay(500).fadeIn(1000);
 });
 
 function getQuote(author, text) {
  
   var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
-
+  
+  // abreviated API call to forismatic.
   $.getJSON(url, function(data) {
     
+    // Adding formated html to quoteText div
     text.html(data.quoteText);
+    
+    // If quote has author element add author with link, else attribute to unkown.
     if (data.quoteAuthor) {
       author.html(data.quoteAuthor);
       author.attr("href", data.quoteLink);
@@ -16,15 +22,17 @@ function getQuote(author, text) {
       author.removeAttr("href");
       author.html("- unknown");
     }
+    // asigning quote and author data to tweetThis variable
     tweetThis = data.quoteText + " - " + data.quoteAuthor;
   });
 }
-
+//function asigning author and text div tags to variables
 $(function() {
   var author = $('#author');
   var text = $('#quoteText');
   getQuote(author, text);
 
+  // asigning getQuote button functionality and fade
   $('#getQuote').click(function(event) {
     $('#hidden').fadeOut('fast');
     getQuote(author, text);
@@ -32,6 +40,7 @@ $(function() {
   })
 });
 
+// link functionality for tweet button
 var tweetThis = "";
 $('#tweetQuote').click(function() {
     $(this).attr("href", "https://twitter.com/intent/tweet?text=" + tweetThis);
